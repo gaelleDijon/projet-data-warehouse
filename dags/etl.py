14cbd96760dd134c8ee8a79b6a71214d9aa24be4
gaelleDijon <gaelledijon@gmail.com>
2023-11-14 20:37:30 +0100

    rename columns urgences records

--- dags/etl.py
+++ dags/etl.py
@@ -56,6 +56,17 @@ def transform_and_load():
     )
     df_urgences = df_urgences.fillna(0)
     df_urgences = df_urgences.rename(columns={"sursaud_cl_age_corona": "code_tranche_age"})
+    df_urgences = df_urgences.rename(columns={
+        "nbre_pass_tot": "pass_tot",
+        "nbre_pass_tot_h": "pass_tot_h",
+        "nbre_pass_tot_f": "pass_tot_f",
+        "nbre_pass_corona": "pass_corona",
+        "nbre_pass_corona_h": "pass_corona_h",
+        "nbre_pass_corona_f": "pass_corona_f",
+        "nbre_hospit_corona": "hospit_corona",
+        "nbre_hospit_corona_h": "hospit_corona_h",
+        "nbre_hospit_corona_f": "hospit_corona_f"
+    })
     df_urgences["date_de_passage"] = pd.to_datetime(df_urgences["date_de_passage"])
     df_urgences["year"] = df_urgences["date_de_passage"].dt.year
     df_urgences["month"] = df_urgences["date_de_passage"].dt.month
